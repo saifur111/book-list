cf3c91624cb48f4609080b8f9c6b9cfbb3550e0c
saifur-rahman98 <mdsaifur.cse.98@gmail.com>
2023-11-28 12:44:58 +0600

    Design contexts &  bookReducer

---
+++ app/reducers/bookReducer.js
@@ -0,0 +1,17 @@
+
+export const bookReducer =(state,action)=>{
+    switch(action.type){
+        case 'ADD_BOOK' :
+            return [
+                ...state,{
+                    id: uuid(),
+                    title : action.book.title,
+                    author: action.book.author,
+                }
+            ]
+            case 'REMOVE_BOOK' :
+                return state.filter(book=>book.id !== action.id);
+            default:
+                return state;
+    }
+}
