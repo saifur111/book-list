08c7155af4e1e7c47f7f9cfca5b8798512213d1a
saifur-rahman98 <mdsaifur.cse.98@gmail.com>
2023-11-29 23:52:39 +0600

    try to solve bug

--- src/app/ui/sidebar/Sidebar.js
+++ src/app/ui/sidebar/Sidebar.js
@@ -17,12 +17,12 @@ const Sidebar = () => {
       </Link>
       <div className="flex flex-wrap gap-2 justify-center md:grow flex-row md:justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
-        <form>
+        <Link href='/'>
           <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-purple-600 md:flex-none md:justify-start md:px-3">
             <ArrowLeftOnRectangleIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
           </button>
-        </form>
+        </Link>
         </div>
     </div>
   )
